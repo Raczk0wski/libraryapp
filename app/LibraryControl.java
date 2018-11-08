d2da37ecb4ec7f4c6baa7de28f3e7a074d82aec9
Raczk0wski <36660255+Raczk0wski@users.noreply.github.com>
2018-11-08 23:49:53 +0100

    Initial commit

---
+++ app/LibraryControl.java
@@ -0,0 +1,62 @@
+package app;
+
+
+import data.Library;
+import utils.DataReader;
+
+public class LibraryControl {
+    // zmienne do kontrolowania programu
+    public final int exit = 0;
+    public final int addBook = 1;
+    public final int printBooks = 2;
+
+    // zmienna do komunikacji z użytkownikiem
+    private DataReader dataReader;
+
+    // "biblioteka" przechowująca dane
+    private Library library;
+
+    public LibraryControl() {
+        dataReader = new DataReader();
+        library = new Library();
+    }
+
+    /*
+     * Główna pętla programu, która pozwala na wybór opcji i interakcję
+     */
+    public void controlLoop() {
+        int option;
+        printOptions();
+        while ((option = dataReader.getInt()) != exit) {
+            switch (option) {
+                case addBook:
+                    addBook();
+                    break;
+                case printBooks:
+                    printBooks();
+                    break;
+                default:
+                    System.out.println("Nie ma takiej opcji, wprowadź ponownie: ");
+            }
+            printOptions();
+        }
+        // zamykamy strumień wejścia
+        dataReader.close();
+    }
+
+    private void printOptions() {
+        System.out.println("Wybierz opcję: ");
+        System.out.println("0 - wyjście z programu");
+        System.out.println("1 - dodanie nowej książki");
+        System.out.println("2 - wyświetl dostępne książki");
+    }
+
+    private void addBook() {
+        data.Book book = dataReader.readAndCreateBook();
+        library.addBook(book);
+    }
+
+    private void printBooks() {
+        library.printBooks();
+    }
+}
